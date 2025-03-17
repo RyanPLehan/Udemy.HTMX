7f2315ef3b072c12af8ab53032b5b4d589b6d01a
Ryan Lehan <RLehan@tql.com>
2025-03-17 16:18:54 -0400

    Advance - Delete: Capture Events to execute JS code

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ app.get('/', (req, res) => {
             id="goal-form" 
             hx-post="/goals" 
             hx-target="#goals"
-            hx-swap="beforeend">
+            hx-swap="beforeend"
+            hx-on:submit="document.querySelector('#goal-form').reset()">
             <div>
               <label htmlFor="goal">Goal</label>
               <input type="text" id="goal" name="goal" />
