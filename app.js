c5afb34fc1db7aa389f5e6e6ceeacbe5abd8c8c1
Ryan Lehan <RLehan@tql.com>
2025-03-17 14:01:30 -0400

    Practice 1 - Added htmx v1 and v2.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Learn HTMX</title>
       <link rel="stylesheet" href="/main.css" />
-      <script src="/htmx.min.js" defer></script>
+      <script src="/htmx.2.0.4.min.js" defer></script>
     </head>
     <body>
       <main>
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
           <form id="goal-form" 
                 hx-post="/goals"
                 hx-target="#goals"
-                hw-swap="beforeend">
+                hw-swap="beforebegin">
             <div>
               <label htmlFor="goal">Goal</label>
               <input type="text" id="goal" name="goal" />
@@ -34,16 +34,7 @@ app.get('/', (req, res) => {
           </form>
         </section>
         <section>
-          <ul id="goals">
-          ${courseGoals.map(
-            (goal, index) => `
-            <li id="goal-${index}">
-              <span>${goal}</span>
-              <button>Remove</button>
-            </li>
-          `
-          )}
-          </ul>
+          <ul id="goals"></ul>
         </section>
       </main>
     </body>
@@ -56,13 +47,31 @@ app.post('/goals', (req, res) => {
     const goalText = req.body.goal;
     courseGoals.push(goalText);
 
-    // since we pushed a new item on the array, then the index is the last item
+    // Since we pushed a new item on the array, then the index is the last item (need to use hx-swap="beforeend")
+    //res.send(`<li id="goal-${courseGoals.length - 1}">${goalText}</li>`);
+
     res.send(`       
         <li id="goal-${courseGoals.length - 1}">
             <span>${goalText}</span>
             <button>Remove</button>
         </li>
     `);
+
+    // List all items (need to use hx-swap="outerHTML")
+    /*
+    res.send(`
+        <ul id="goals">
+        ${courseGoals.map(
+        (goal, index) => `
+        <li id="goal-${index}">
+            <span>${goal}</span>
+            <button>Remove</button>
+        </li>
+        `
+        )}
+        </ul>
+    `);
+    */
 });
 
 app.listen(3000);
