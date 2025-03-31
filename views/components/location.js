0bf9b3947db70ceee457b53e1e74062801f4bcdc
Ryan Lehan <RLehan@tql.com>
2025-03-31 16:00:30 -0400

    Advance - Copying items between locations with dynamic attributes

--- views/components/location.js
+++ views/components/location.js
@@ -1,11 +1,27 @@
-export default function renderLocation(location) {
-  return `
-    <li class="location-item">
-      <button 
+export default function renderLocation(location, isAvailableLocation = true) {
+  let attributes;
+
+  if (isAvailableLocation) {
+    attributes = `
         hx-post="/places" 
         hx-vals='{"locationId": "${location.id}"}'
         hx-target="#interesting-locations"
-        hx-swap="beforeend">
+        hx-swap="beforeend"
+    `;
+  } 
+  else {
+    attributes = `
+      hx-delete="/places/${location.id}"
+      hx-confirm="Are you sure?"
+      hx-target="#location-${location.id}"
+      hx-swap="outerHTML"
+    `;
+  }
+
+
+  return `
+    <li class="location-item" id="location-${location.id}">
+      <button ${attributes}>
         <img src="${`/images/${location.image.src}`}" alt="${location.image.alt}" />
         <h3>${location.title}</h3>
       </button>
